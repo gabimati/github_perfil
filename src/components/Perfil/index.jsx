938791476c8978a3564e6efebc0c314f7b700776
Gabriela Mameri <gabimameri@Laptop-de-Gabriela.local>
2024-10-11 14:05:07 -0300

    commit inicial

---
+++ src/components/Perfil/index.jsx
@@ -0,0 +1,14 @@
+import styles from './Perfil.module.css'
+
+const Perfil = ({ nomeUsuario }) => {
+    return (
+        <header className={styles.header}>
+            <img className={styles.avatar} src={`http://github.com/${nomeUsuario}.png`} />
+            <h1 className={styles.name}>
+                {nomeUsuario}
+            </h1>
+        </header>
+    )
+}
+
+export default Perfil;
